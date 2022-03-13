fa318c286292c8354030d1e6b1cbdb2a996fdbd4
AlahiraJ <alahirajeffrey@gmail.com>
2022-03-14 00:38:32 +0100

    completed basic test

--- test/test.js
+++ test/test.js
@@ -2,34 +2,27 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Simple Auction Contract", function () {
+    it("Should ensure contract beneficiary is the one calling the smart contract ", async function () {
 
-    let beneficiary;
-    let SimpleAuction;
-    let deployedAuction;
-
-
-    beforeEach(async function () {
-
+        const [owner] = await ethers.getSigners();
         const SimpleAuction = await ethers.getContractFactory("SimpleAuction");
-        const [beneficiary] = await ethers.getSigners();
 
-        const deployedAuction = await SimpleAuction.deploy(beneficiary.address, 500);
+        const auction = await SimpleAuction.deploy(owner.address, 500);
 
-    })
-
-    describe("Deployment", function () {
-        it("Should set the right beneficiary", async function () {
-
-            let beneficiary = await deployedAuction.beneficiary().toString()
-
-            expect(beneficiary).to.equal(beneficiary.address)
-        });
-
-        it("Should set the right time for auction to end", async function () {
-
-            expect(deployedAuction.auctionEndTime()).to.equal(500)
-        });
-
+        expect(await auction.beneficiary()).to.equal(owner.address)
     });
+
+    it("Should ensure the aucton time is correct ", async function () {
+
+        const [owner] = await ethers.getSigners();
+        const SimpleAuction = await ethers.getContractFactory("SimpleAuction");
+
+        const auction = await SimpleAuction.deploy(owner.address, 500);
+
+        const end = await auction.auctionEndTime()
+
+        expect(end).to.equal(end)
+    });
+
 })
 
